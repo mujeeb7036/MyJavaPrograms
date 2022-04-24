d4f58d475f7e1bb4b93c80abb9e8e504dfec32fa
mujeeb7036 <mujeeb7036@gmail.com>
2022-04-24 14:49:19 +0530

    1

---
+++ src/com/company/MUJ_CH1_PS.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class MUJ_CH1_PS {
+
+    public static void main(String[] args) {
+
+        //Question 1
+      // int a = 15 ;
+        //int b = 20 ;
+       // int c = 25 ;
+       // int sum = a + b + c ;
+        // System.out.println(sum);
+
+        //Question 2
+        //float sub1 = 98.2f;
+        //float sub2 = 90.9f;
+        //float sub3 = 100f;
+
+        //float CGPA = (sub1 + sub2 + sub3)/30;
+        //System.out.println(CGPA);
+
+        //Question 3
+        //Scanner sc = new Scanner(System.in);
+        //System.out.println("Please Enter Your Name : ");
+        //String nm = sc.nextLine();
+        //System.out.println("Hello "+nm.toUpperCase()+" Have a Good Day");
+
+        //Question 4
+        //double Km = 500;
+        //double miles = (Km* 0.621371);
+        //System.out.println(miles);
+
+        //Question 5
+        System.out.println("Enter your number : ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+    }
+}
---
+++ src/com/company/MUJ_CH2_Operators.java
@@ -0,0 +1,57 @@
+package com.company;
+
+public class MUJ_CH2_Operators {
+    public static void main(String[] args) {
+//    Arithmetic Operator's
+//        int a = 10;
+//        int b = 324;
+//        int sum = a + b;
+//        System.out.println(sum);
+//
+//        float c = 323.5f;
+//        float d = 323.2f;
+//        float sub = c-d;
+//        System.out.println(sub);
+//
+//        int e = 934;
+//        int f = 3;
+//        int mul = e*f;
+//        System.out.println(mul);
+//
+//        int g = 3223;
+//        int h = 33;
+//        int div = g/h;
+//        System.out.println(div);
+//
+//        int i = 933;
+//        int j = 20;
+//        int per = i%j;
+//        System.out.println(per);
+//
+//        Assignment Operators
+//         we can use *=,-+ and many more
+//
+//        int k = 45;
+//        k += 15;
+//        System.out.println(k);
+//
+//        Comparison Operators
+//        System.out.println(60>=45);
+//
+//        Logical Operators  ||,!
+//        System.out.println(60>15 && 70<65);
+//
+//        Bitwise Operators &,|
+//        System.out.println(2&3);
+
+
+
+
+
+
+
+
+
+
+    }
+}
